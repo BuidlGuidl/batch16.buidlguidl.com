9cf6bcd663db298b573510d4d1206f7772414d02
Armolas <armolas06@gmail.com>
2025-05-16 21:57:58 +0100

    Revert "Merge branch 'main' into Muritadhor"
    This reverts commit 338850712702b53cf9ddf9232b076271b014db40, reversing
    changes made to eef276767cd3f829e200ff586c883379485f92c1.
    
    Revert "Merge branch 'main' into Muritadhor"
    
    This reverts commit 338850712702b53cf9ddf9232b076271b014db40, reversing
    changes made to eef276767cd3f829e200ff586c883379485f92c1.

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -9,25 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/dxswouxj5/**",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
